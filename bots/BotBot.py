e18af8d9ecb3fc86f04a65904bea74bbf7764d4b
Adam <adamkimbrough@users.noreply.github.com>
2014-07-06 14:10:53 -0400

    Update BotBot.py

--- bots/BotBot.py
+++ bots/BotBot.py
@@ -38,7 +38,47 @@ class BotBot(pyrc.Bot):
     "will say something"
     self.message("#turntechgodhead", "stop repeating myself")
 '''
+
+class BotBot_(pyrc.Bot):
+  @hooks.command()
+  def info(self, target, sender):
+    "will print the target and sender to the console"
+    print("target: %s, sender: %s" % (target, sender))
+
+  @hooks.command()
+  def bling(self, target, sender):
+    "will print yo"
+    if target.startswith("#"):
+      self.message(target, "%s: yo" % sender)
+    else:
+      self.message(sender, "yo")
+
+  @hooks.command("^repeat\s+(?P<msg>.+)$")
+  def repeat(self, target, sender, **kwargs):
+    "will repeat whatever yo say"
+    if target.startswith("#"):
+      self.message(target, kwargs["msg"])
+    else:
+      self.message(sender, kwargs["msg"])
+'''
+  @hooks.privmsg("(lol|lmao|rofl(mao)?)")
+  def nolol(self, target, sender, *args):
+    """
+    will reprimand user for saying 'lol', 'lmao, 'rofl' or 'roflmao' when seen in a message
+    only applies to channel messages
+    """
+    if target.startswith("#"):
+      self.message(target, "%s, %s is a no-%s zone." % (sender, target, args[0].upper()))
+'''
+'''  
+  @hooks.interval(10000)
+  def keeprepeating(self):
+    "will say something"
+    self.message("#turntechgodhead", "stop repeating myself")
+'''
 if __name__ == '__main__':
   #bot = BotBot('irc.freenode.net', channels = ['#turntechgodhead'])
-  bot = BotBot('irc.geekshed.net', channels = ['#redditnet'])
+  #bot = BotBot('irc.geekshed.net', channels = ['#redditnet'])
+  #bot.connect()
+  bot = BotBot_('irc.freenode.net', channels = ['#turntechgodhead'])
   bot.connect()
